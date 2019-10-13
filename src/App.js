b4e9968949cfd36fbcace674616c7bf4c327e673
kunal15abc <kunal15abc@gmail.com>
2019-10-14 00:06:59 +0530

    Added Searchbar for quick filter

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import {HeaderBar,SideBar,Cards} from 'react-dark-elixir'
+import {HeaderBar,SideBar,Cards,SearchBar} from 'react-dark-elixir'
+import cardItems from './data/cardItems.json'
+import headerItems from './data/headerItems.json'
+import sidebarItems from './data/sidebarItems.json'
 import './App.css';
 import {createHashHistory} from 'history'
 var history = createHashHistory()
-
-var items = [{
-  name: "Home",
-  uri: "/home"
-},{
-  name: "Items",
-  uri: "/items"
-}]
-
-var sideItems = [{
-  name: "Home1",
-  uri: "/home2"
-},{
-  name: "Items",
-  list: [{
-    name: "Item1",
-    uri: "/item1"
-  },{
-    name: "Item2",
-    uri: "/item2"
-  },{
-    name: "Item3",
-    uri: "/item3"
-  }]
-},{
-  name: "Pages",
-  list: [{
-    name: "Page1",
-    uri: "page1"
-  },{
-    name: "Page2",
-    uri: "page2"
-  }]
-}]
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
       active: '',
-      showSidebar: true
+      showSidebar: true,
+      filteredItems: cardItems
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e){
+    const value = e.target.value;
+    const filteredItems = value!==undefined || value!==null || value!=='' ? cardItems.filter(function(cardItem){
+      return (cardItem.itemName.toLowerCase().includes(value.toLowerCase()))
+    }) : cardItems
+
+    this.setState({filteredItems: filteredItems})
   }
 
   componentDidMount(){
@@ -63,53 +43,22 @@ class App extends React.Component {
                       onChange={(item) => {history.push(item.uri);this.setState({active: item.name})}}
                       user={"Kunal"}/>
           <div className="content-container">
-            <SideBar items={sideItems}
+            <SideBar items={sidebarItems}
                       showSidebar={this.state.showSidebar}
                       active={this.state.active}
                       onChange={(item) => {history.push(item.uri);this.setState({active: item.name})}}/>
             {/* <Cards  itemName = {"Fish"}
                     itemPrice = {"10"} 
                     itemDetails = {"Detailed Description is here. Please provide more details bla bla bla bla"} /> */}
+            <div className={"view-container"}>
+            <div className={"searchbar-container"}>
+              <SearchBar inputItems={cardItems} handleInputChange={this.handleInputChange} />
+            </div>
             <div className={"cards-container"}>
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
-              <Cards  itemName = {"Air Pump"}
-                      itemPrice = {"130"} 
-                      itemDetails = {"6W air pump to supply fresh air in tanks. With the help of diffuser (air stone) it creates agitation in the tank."} />
+              {this.state.filteredItems.map(function(filteredItem){
+                return (<Cards itemName={filteredItem.itemName} itemPrice={filteredItem.itemPrice} itemDetails={filteredItem.itemDetails} />)
+              })}
+            </div>
             </div>
           </div>
         </div>
