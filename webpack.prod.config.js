b2b57281fc1273e0ff10876a2e378cc2e4ecd30b
tunedinout <narottam2911@gmail.com>
2019-10-21 19:07:43 +0530

    debugging with build logging

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         test: /\.(js|jsx)$/,
-        // include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
