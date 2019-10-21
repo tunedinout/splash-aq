588052788be6574759f87aa3629a8ed9a0b8d627
tunedinout <narottam2911@gmail.com>
2019-10-21 19:40:07 +0530

    using buildspec codebuild

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -29,7 +29,6 @@ module.exports = {
       },
       {
         test: /\.(js|jsx)$/,
-        include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
