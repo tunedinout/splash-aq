77cea41005b1e33f0947017dfcbd1b8c822ba24b
tunedinout <narottam2911@gmail.com>
2019-10-23 02:44:59 +0530

    chng

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/build/',
+    //publicPath: '/build/',
     filename: 'index.js'
   },
 
