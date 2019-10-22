204e40bdc819b14108ea58afea3060746aa811a0
tunedinout <narottam2911@gmail.com>
2019-10-23 02:51:13 +0530

    chng removed include path resolve for js test

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       {
         test: /\.js$/,
         //include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
           options: {
